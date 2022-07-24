905c7618b2694e639855d1609b889771f00c8c32
sriVishal-VhiTechLab <vishalvhitech@gmail.com>
2022-07-24 22:40:57 +0530

    BackUp

--- EXCERCISE-120/design.js
+++ EXCERCISE-120/design.js
@@ -1,8 +1,7 @@
 mainFunction = () => {
       debugger;
       let usrData = readFormData();
-      let result = insertData(usrData);
-      console.log(result);
+      renderTable(usrData);
 }
 readFormData = () => {
       let FormData = {};
@@ -11,4 +10,18 @@ readFormData = () => {
       FormData["mailId"] = document.getElementById("mailInp").value;
       FormData["salary"] = document.getElementById("salInp").value;
       return FormData;
+}
+renderTable = (obj) => {
+      let crudTable = document.getElementById("crudBody");
+      let crudRow = crudTable.insertRow(0);
+      let nameCell = crudRow.insertCell(0);
+      let occupCell = crudRow.insertCell(1);
+      let salCell = crudRow.insertCell(2);
+      let mailCell = crudRow.insertCell(3);
+      nameCell.innerHTML = obj.fullName;
+      occupCell.innerHTML = obj.Occupation;
+      salCell.innerHTML = obj.salary;
+      mailCell.innerHTML = obj.mailId;
+      let editCell = crudRow.insertCell(4);
+      editCell.innerHTML = "<a>EDIT</a><a>DELETE</a>";
 }
