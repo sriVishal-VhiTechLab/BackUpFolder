83c044d8a9f5a9946f41e1f0c751101012c12771
sriVishal-VhiTechLab <vishalvhitech@gmail.com>
2022-07-26 19:33:04 +0530

    BackUp

--- EXCERCISE-120/design.js
+++ EXCERCISE-120/design.js
@@ -1,5 +1,4 @@
 mainFunction = () => {
-      debugger;
       let usrData = readFormData();
       renderTable(usrData);
 }
@@ -23,5 +22,31 @@ renderTable = (obj) => {
       salCell.innerHTML = obj.salary;
       mailCell.innerHTML = obj.mailId;
       let editCell = crudRow.insertCell(4);
-      editCell.innerHTML = "<a>EDIT</a><a>DELETE</a>";
+      editCell.innerHTML = '<button class="btn btn-success edit" onclick="editData(this)">EDIT</button><button onclick="deleteData()" class="btn btn-danger del">DELETE</a>';
+}
+deleteData = () => {
+      let confirm = window.confirm("Are You You Want Delete The Data In The Current Row?");
+      if (confirm === true) {
+            let deleteOperation = document.getElementById("crudBody");
+            let deleteCurrent = deleteOperation.deleteRow(0);
+            alert("Deleted SuccessFully.");
+      }
+}
+switchTheme = () => {
+      let dTheme = document.body;
+      dTheme.classList.toggle("themeIcon");
+}
+editData = (td) => {
+      let confirmEdit = window.confirm("Do You Want To make Changes In your Data!");
+      if (confirmEdit === true) {
+            let currentRow = td.parentElement.parentElement;
+            console.log(currentRow);
+            document.getElementById("nameInp").value = currentRow.cells[0].innerHTML;
+            document.getElementById("occupInp").value = currentRow.cells[1].innerHTML;
+            document.getElementById("mailInp").value = currentRow.cells[2].innerHTML;
+            document.getElementById("salInp").value = currentRow.cells[3].innerHTML;
+            alert("Now You Can Edit Your Data In Specific Fields");
+            let deleteAndEdit = document.getElementById("crudBody");
+            deleteAndEdit.deleteRow(0);
+      }
 }
