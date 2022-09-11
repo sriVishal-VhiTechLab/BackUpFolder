74fa9877b4ed012ffd77908f239ac97980db9e68
sriVishal-VhiTechLab <vishalvhitech@gmail.com>
2022-09-11 16:50:25 +0530

    BackUp

--- EXCERCISE-120/script.js
+++ EXCERCISE-120/script.js
@@ -1,39 +1,39 @@
-// Get User Data
-let usrFullName = document.getElementById("usrFullName").value;
-let usrDob = document.getElementById("usrDob").value;
-let usrCountry = document.getElementById("usrCountry").value;
-let usrOrg = document.getElementById("usrOrg").value;
-let usrMobNum = document.getElementById("usrMobNum").value;
-let usrState = document.getElementById("usrState").value;
-let usrPinCode = document.getElementById("usrPinCode").value;
-let usrMail = document.getElementById("usrMail").value;
-let usrComAddress = document.getElementById("comAddress").value;
-let usrPermAddress = document.getElementById("permanentAddress").value;
-copyTxtAddress = () => {
-      usrPermAddress.innerHTML = usrComAddress;
-}
-let isRequired = value => value === '' ? false : true;
-let arrOfInp = [usrFullName, usrDob, usrCountry, usrOrg, usrMobNum, usrState, usrPinCode, usrMail, usrComAddress];
-let cancelBtn = document.getElementById("regBtn").addEventListener('click', () => {
-      arrOfInp.forEach(element => {
-            if (isRequired(element) === true) {
-            
-            }
-            else {
-                  element.style.border = "2px solid red";
-            }
+mainFunction = () => {
+      // Get User Data
+      let usrFullName = document.getElementById("usrFullName").value;
+      let usrDob = document.getElementById("usrDob").value;
+      let usrCountry = document.getElementById("usrCountry").value;
+      let usrOrg = document.getElementById("usrOrg").value;
+      let usrMobNum = document.getElementById("usrMobNum").value;
+      let usrState = document.getElementById("usrState").value;
+      let usrPinCode = document.getElementById("usrPinCode").value;
+      let usrMail = document.getElementById("usrMail").value;
+      let usrComAddress = document.getElementById("comAddress").value;
+      let usrPermAddress = document.getElementById("permanentAddress").value;
+      const isRequiredRegex = /^$/;
+      const arrOfInputs = [usrFullName, usrDob, usrCountry, usrOrg, usrMobNum, usrState, usrPinCode, usrMail, usrComAddress];
+      console.log(arrOfInputs);
+      const arrOfInp = arrOfInputs.some(element => {
+            return isRequiredRegex.test(element);
       });
-      // let indicators = document.querySelectorAll('span');
-      // indicators.forEach(element => {
-      //       if (isRequired(usrFullName) === true) {
-      //             element.style.color = "red";
-      //       }
-      //       else {
-      //             element.style.color = "color";
-      //       }
-      // });
+      console.log(arrOfInp);
+      if (arrOfInp === true) {
+            let dataEmptyModal = document.querySelector('.emptyModal');
+            dataEmptyModal.showModal();
+            document.querySelector('#emptyModClose').addEventListener('click', () => {
+                  dataEmptyModal.close();
+            })
+      }
+      else {
+            let dataFilledModal = document.querySelector('.updateModal');
+            dataFilledModal.showModal();
+            document.querySelector('#dataFilledModalClose').addEventListener('click', () => {
+                  dataFilledModal.close();
+            })
 
-})
+      }
+}
+const isRequired = value => value === '' ? false : true;
 // ---USER PIC UPLOAD-------
 let getUserImg = document.getElementById("usrImgUpload");
 uploadUserImage = () => {
@@ -50,7 +50,7 @@ getUserImg.addEventListener("change", function () {
 // --------------------! UTILITY FUNCTIONS AND BASIC VALIDATIONS------------
 checkUsrName = (name) => {
       nameRegex = /^[A-Za-z]{5,16}$/;
-      if (nameRegex.test(name)) {
+      if (nameRegex.test(name) && !isRequired(name)) {
             document.getElementById("usrFullName").style.border = "2px solid green";
       }
       else {
@@ -59,7 +59,7 @@ checkUsrName = (name) => {
 }
 checkUsrEmail = (mailId) => {
       mailRegex = /^[a-zA-Z0-9]+@[a-zA-z]+\.[a-zA-Z]{2,4}$/;
-      if (mailRegex.test(mailId)) {
+      if (mailRegex.test(mailId) && !isRequired(mailId)) {
             document.getElementById("usrMail").style.border = "2px solid green";
             return true;
       }
@@ -70,7 +70,7 @@ checkUsrEmail = (mailId) => {
 }
 checkMobNumber = (mobNum) => {
       rMobNumRegex = /^[6-9][0-9]{9,12}$/;
-      if (rMobNumRegex.test(mobNum)) {
+      if (rMobNumRegex.test(mobNum) && !isRequired(mobNum)) {
             document.getElementById("usrMobNum").style.border = "2px solid green";
             return true;
       }
@@ -81,7 +81,7 @@ checkMobNumber = (mobNum) => {
 }
 checkPinCode = (code) => {
       pinCodeRegex = /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/;
-      if (isRequired(code) && pinCodeRegex.test(code)) {
+      if (pinCodeRegex.test(code) && !isRequired(code)) {
             document.getElementById("usrPinCode").style.border = "2px solid green";
             return true;
       }
